0d6b283b40d02f6b8a39a8fb4add18018ceb546f
Casey Medford <case_med@hotmail.com>
2020-04-30 12:26:07 -0500

    Added the Sign In and Out Components and worked on the fact detect feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 
 const app = new Clarifai.App({
  apiKey: '10365751e643431cb0cd6cdaf88a69d7'
@@ -17,8 +19,28 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageURL: ''
+      imageURL: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box});
   }
 
   onInputChange = (event) => {
@@ -31,31 +53,43 @@ class App extends Component {
     this.setState({imageURL: this.state.input});
     app.models
       .predict(
-        Clarifai.COLOR_MODEL,
+        Clarifai.FACE_DETECT_MODEL,
          this.state.input)
-      .then(
-      function(response) {
-        // do something with response
-        console.log(response)
-      },
-      function(err) {
+      .then( response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
         // there was an error
-      }
-    );
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
   render() {
+    const { isSignedIn, imageURL, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles"/>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButonSubmit}
-        />
-        <FaceRecognition imageURL={this.state.imageURL} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+          ? <div> <Logo />
+            <Rank />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButonSubmit}
+            />
+            <FaceRecognition box={box} imageURL={imageURL} />
+            </div>
+          : (
+            route == 'signin' 
+            ? <SignIn onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange}/>
+          ) 
+      }
       </div>
     );
   }
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
     return(
-        <div className='center'>
-            <img alt='' src={imageURL}></img>
+        <div className='center ma'>
+            <div className="absolute mt2">
+            <img id='inputimage' alt='' src={imageURL} width='500px' height='auto'></img>
+            <div className='bounding-box' style={{left: box.leftCol, top: box.topRow, right: box.rightCol, bottom: box.bottomRow}}></div>
+            </div>
+            
         </div>
     );
 }
